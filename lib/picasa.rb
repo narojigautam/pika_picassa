59fee351e8c473a81d3e70579d4fe92ce378dba8
Gautam Naroji <narojigautam@gmail.com>
2014-09-14 22:13:19 +0530

    WIP

--- lib/picasa.rb
+++ lib/picasa.rb
@@ -16,9 +16,18 @@ class Picasa
   end
 
   def get_album album_id
+    binding.pry
     pics_response = self.class.get(pics_url_for(album_id), {token: @token})
     # an album is complete if it has pics data as well as its own data
     parse_into_complete_album(pics_response)
+  end
+
+  def comment_on album_id, pic_id, comment
+    self.class.post(
+      post_comment_url_for(album_id, pic_id),
+      body: form_comment_req(comment),
+      headers: post_req_headers
+    )
   end
 
   private
@@ -29,5 +38,24 @@ class Picasa
 
   def pics_url_for album_id
     "/data/feed/api/user/#{@user_id}/albumid/#{album_id}"
+  end
+
+  def post_comment_url_for album_id, pic_id
+    "/data/feed/api/user/#{@user_id}/albumid/#{album_id}/photoid/#{pic_id}"
+  end
+
+  def form_comment_req comment
+    "<entry xmlns='http://www.w3.org/2005/Atom'>" +
+    "<content>#{comment}</content>" +
+    "<category scheme='http://schemas.google.com/g/2005#kind'" +
+    " term='http://schemas.google.com/photos/2007#comment'/></entry>"
+  end
+
+  def post_req_headers
+    {
+      "GData-Version" => "2",
+      "Content-Type" => "application/atom+xml",
+      "Authorization" => "OmniAuth.token=\"#{@token}\""
+    }
   end
 end
